38765c1ca385e44a58258339f02b1d6d0bf8047c
Daniel Kogler <danielk373@gmail.com>
2021-08-06 14:48:55 -0500

    created setup script for IoT

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# iot-setup
-Setup script and instructions for the IoT course
+# Set up your Github / Gitpod Workspace
+
+Below, you will find instructions for creating a github repository and setting up your Gitpod workspace. Read all instructions carefully before moving on. If you get stuck, encounter an issue, or simply need help, reach out to your instructor immediately.
+
+## 0) Pre-requisites:
+
+Before continuing on to the next steps, make sure that you have done the following:
+* Created a personal email account on Gmail.
+* Created an account on Greenlight
+* Created an account on GitHub
+* Created an account on Gitpod
+* Joined the Operation Spark GitHub Team
+
+All of the above should already have been done if you are in the advanced course, but if they are not, see the <a href="https://github.com/OperationSpark/fsd-setup">fsd-setup instructions</a> for how to do so.
+
+## 1) Install your projects
+
+Open your Gitpod workspace. With your workspace open, find the bash terminal and enter these commands one at a time:
+* `git clone https://github.com/OperationSpark/iot-setup.git`
+* `chmod +x iot-setup/setup.sh`
+* `./iot-setup/setup.sh`
+* `rm -rf ./iot-setup`
+
+Enter your username and your password.
+
+## 2) Push your code
+
+Down in the bash terminal, enter these commands:
+
+`git add -A`
+
+`git commit -m "adding IoT projects"`
+
+`git push`
---
+++ setup.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+read -p "Enter Your GitHub Username: "  username
+read -s -p "Enter Your GitHub Password: "  password
+
+printf "\n"
+
+if test -e iot-projects; then
+    rm -rf iot-projects
+fi
+
+mkdir iot-projects
+cd iot-projects
+
+git clone https://${username}:${password}@github.com/operationspark/underpants-micro underpants-micro
+git clone https://${username}:${password}@github.com/operationspark/test-guru test-guru
+git clone https://${username}:${password}@github.com/operationspark/are-you-being-served are-you-being-served
+git clone https://${username}:${password}@github.com/operationspark/interface-inyourface interface-inyourface
+git clone https://${username}:${password}@github.com/operationspark/hardware-tests hardware-tests
+git clone https://${username}:${password}@github.com/operationspark/first-slice-of-pi first-slice-of-pi
+git clone https://${username}:${password}@github.com/operationspark/second-slice-of-pi second-slice-of-pi
+git clone https://${username}:${password}@github.com/operationspark/pi-inyourface pi-inyourface
+git clone https://${username}:${password}@github.com/operationspark/iot-automatic-routing automatic-routing
+
+if ! test -e underpants-micro || ! test -e test-guru || ! test -e are-you-being-served || ! test -e interface-inyourface || ! test -e hardware-tests || ! test -e first-slice-of-pi || ! test -e second-slice-of-pi || ! test -e pi-inyourface || ! test -e automatic-routing; then
+    printf "\nFAILURE: Some projects could not be accessed on GitHub. Please run the script again."
+else
+    #underpants-micro
+    cd underpants-micro
+    rm -rf .git* .master
+
+    #test-guru
+    cd ../test-guru
+    rm -rf .git* .master
+
+    #interface-inyourface
+    cd ../interface-inyourface
+    rm -rf .git* .master
+
+    #hardware-testing
+    cd ../hardware-testing
+    rm -rf .git* .master
+
+    #first-slice-of-pi
+    cd ../first-slice-of-pi
+    rm -rf .git* .master
+
+    #second-slice-of-pi
+    cd ../second-slice-of-pi
+    rm -rf .git* .master
+
+    #pi-inyouface
+    cd ../pi-inyourface
+    rm -rf .git* .master
+
+    #automatic-routing
+    cd ../automatic-routing
+    rm -rf .git* .master
+
+    #are-you-being-served
+    cd ../are-you-being-served
+    rm -rf .git* .master
+
+    printf "\nSUCCESS: All projects successfully installed."
+fi
