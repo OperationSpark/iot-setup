6c77c4c47296bf2916d640317bad244b036485f1
Daniel Kogler <danielk373@gmail.com>
2025-01-28 15:44:48 -0600

    updated setup script for 2025

--- setup.sh
+++ setup.sh
@@ -10,17 +10,17 @@ mkdir iot-projects
 cd iot-projects
 
 git clone https://github.com/operationspark/underpants-micro underpants-micro
-git clone https://github.com/operationspark/test-guru test-guru
 git clone https://github.com/operationspark/are-you-being-served are-you-being-served
+git clone https://github.com/operationspark/catch-server catch-server
 git clone https://github.com/operationspark/interface-inyourface interface-inyourface
 git clone https://github.com/operationspark/hardware-setup hardware-setup
-git clone https://github.com/operationspark/hardware-tests hardware-tests
+# git clone https://github.com/operationspark/hardware-tests hardware-tests
 git clone https://github.com/operationspark/first-slice-of-pi first-slice-of-pi
 git clone https://github.com/operationspark/second-slice-of-pi second-slice-of-pi
 git clone https://github.com/operationspark/pi-inyourface pi-inyourface
 git clone https://github.com/operationspark/iot-automatic-routing automatic-routing
 
-if ! test -e underpants-lite || ! test -e test-guru || ! test -e are-you-being-served || ! test -e interface-inyourface || ! test -e hardware-setup || ! test -e hardware-tests || ! test -e first-slice-of-pi || ! test -e second-slice-of-pi || ! test -e pi-inyourface || ! test -e automatic-routing; then
+if ! test -e underpants-micro || ! test -e catch-server || ! test -e are-you-being-served || ! test -e interface-inyourface || ! test -e hardware-setup || ! test -e first-slice-of-pi || ! test -e second-slice-of-pi || ! test -e pi-inyourface || ! test -e automatic-routing; then
     printf "\nFAILURE: Some projects could not be accessed on GitHub. Please run the script again."
 else
   # Check for existing project-instructions folder, creates directory if missing
@@ -29,23 +29,23 @@ else
   fi
 
   # Copy READMEs for projects into project-instructions dir
-  cp underpants-lite/README.md ../project-instructions/underpants-lite.md
-  cp test-guru/README.md ../project-instructions/test-guru.md
+  cp underpants-micro/README.md ../project-instructions/underpants-micro.md
+  cp catch-server/README.md ../project-instructions/catch-server.md
   cp are-you-being-served/README.md ../project-instructions/are-you-being-served.md
   cp interface-inyourface/README.md ../project-instructions/interface-inyourface.md
   cp hardware-setup/README.md ../project-instructions/hardware-setup.md
-  cp hardware-tests/README.md ../project-instructions/hardware-tests.md
+  # cp hardware-tests/README.md ../project-instructions/hardware-tests.md
   cp first-slice-of-pi/README.md ../project-instructions/first-slice-of-pi.md
   cp second-slice-of-pi/README.md ../project-instructions/second-slice-of-pi.md
   cp pi-inyourface/README.md ../project-instructions/pi-inyourface.md
   cp automatic-routing/README.md ../project-instructions/automatic-routing.md
 
   #underpants-lite
-  cd underpants-lite
+  cd underpants-micro
   rm -rf .git* .master
 
   #test-guru
-  cd ../test-guru
+  cd ../catch-server
   rm -rf .git* .master
 
   #are-you-being-served
@@ -58,10 +58,6 @@ else
 
   #hardware-setup
   cd ../hardware-setup
-  rm -rf .git* .master
-
-  #hardware-tests
-  cd ../hardware-tests
   rm -rf .git* .master
 
   #first-slice-of-pi
